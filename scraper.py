0612bb41c736dcedb785c7f7b67489b94cd1bf11
Colin Broderick <rusty@therustyproject.com>
2016-06-17 00:30:38 +0200

    made changes

--- scraper.py
+++ scraper.py
@@ -8,16 +8,16 @@ os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
 from twisted.internet import reactor
 from scrapy.crawler import Crawler
-from scrapy import log, signals
+# from scrapy import log, signals
 from phr_scrapers.spiders.trollies import TrolliesSpider
 from scrapy.utils.project import get_project_settings
 
 spider = TrolliesSpider()
 settings = get_project_settings()
 crawler = Crawler(settings)
-crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+# crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
 crawler.configure()
 crawler.crawl(spider)
 crawler.start()
-log.start()
+# log.start()
 reactor.run()
